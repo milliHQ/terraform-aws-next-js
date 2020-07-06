409ac2fcbf10f8a3fa03de6b057697b6260bfed4
Felix Haus <felixhaus@gmail.com>
2020-07-06 12:01:55 +0200

    v0.0.2

--- modules/statics-deploy/main.tf
+++ modules/statics-deploy/main.tf
@@ -95,7 +95,7 @@ module "lambda_content" {
   source = "../file-from-npm"
 
   module_name    = "@dealmore/terraform-next-deploy-trigger"
-  module_version = "0.0.1"
+  module_version = "0.0.2"
   path_to_file   = "dist.zip"
   use_local      = var.debug_use_local_packages
 }
