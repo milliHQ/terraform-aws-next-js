5eac32d095a25bfa2c477d760ebfb4cdc92a2aa9
Matthew Elphick <matt.a.elphy@gmail.com>
2021-01-13 14:02:32 +0000

    feat: Expose proxy hosted zone id
    This allows more complicated aliasing that currently possible with just the name exposed. Having both the domain name and hosted zone id will allow us to punch it into the alias on our own external `aws_route53_record` for a more custom approach to custom domains. The main reason for this is that we currently have different providers in different regions for our infrastructure and managing dns/route53, which as the providers differ means we can't use the currently available approach to custom domains.

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,8 @@ output "cloudfront_domain_name" {
   description = "The domain of the main CloudFront distribution."
   value       = module.proxy.cloudfront_domain_name
 }
+
+output "cloudfront_hosted_zone_id" {
+  description = "The zone id of the main CloudFront distribution."
+  value       = module.proxy.cloudfront_hosted_zone_id
+}
