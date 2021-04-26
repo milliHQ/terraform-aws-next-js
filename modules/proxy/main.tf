5b02303f990c7baeadf3fcedd7e928bf454619e9
Felix Haus <ofhouse@users.noreply.github.com>
2021-04-26 20:58:29 +0200

    Bump AWS Lambda Terraform module from 1.34.0 to 1.47.0 (#117)

--- modules/proxy/main.tf
+++ modules/proxy/main.tf
@@ -40,7 +40,7 @@ resource "random_id" "function_name" {
 
 module "edge_proxy" {
   source  = "terraform-aws-modules/lambda/aws"
-  version = "1.34.0"
+  version = "1.47.0"
 
   lambda_at_edge = true
 
