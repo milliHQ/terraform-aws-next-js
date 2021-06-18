378b9438077156ae88793eba1f9306a597cf6fc6
Felix Haus <felixhaus@gmail.com>
2021-06-18 21:59:42 +0200

    proxy: detect language

---
+++ packages/proxy/src/util/detect-locale.ts
@@ -0,0 +1,35 @@
+import { CloudFrontHeaders } from 'aws-lambda';
+import { Source } from '@vercel/routing-utils';
+import { language as parseAcceptLanguage } from '@hapi/accept';
+
+import { extractCookie } from './extract-cookies';
+
+interface DetectLocaleOptions {
+  locale: Exclude<Source['locale'], undefined>;
+}
+
+export function detectLocale(
+  headers: CloudFrontHeaders,
+  { locale }: DetectLocaleOptions
+): string | null {
+  // Check if the locale was set previously
+  if (locale.cookie) {
+    const localeFromCookie = extractCookie(headers, locale.cookie);
+
+    if (localeFromCookie !== null) {
+      return localeFromCookie;
+    }
+  }
+
+  // Check if we can detect the locale from headers
+  const acceptPreferredLanguageHeader = headers['accept-language'];
+  if (acceptPreferredLanguageHeader?.length > 0) {
+    const languageKeys = locale.redirect ? Object.keys(locale.redirect) : [];
+    return parseAcceptLanguage(
+      acceptPreferredLanguageHeader[0].value,
+      languageKeys
+    );
+  }
+
+  return null;
+}
