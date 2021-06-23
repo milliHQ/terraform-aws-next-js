0ce243210d8bb556e22763a6624e1b7d9118fc84
Felix Haus <felixhaus@gmail.com>
2021-06-23 21:29:03 +0200

    test: Check static files on e2e tests

--- jest.config.js
+++ jest.config.js
@@ -3,16 +3,4 @@ module.exports = {
   testEnvironment: 'node',
   rootDir: './',
   globalSetup: '<rootDir>/test/jest.setup.ts',
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.test.json',
-    },
-  },
-  // By default we only run tests in the packages subfolders
-  // For e2e tests see the jest.e2e.config.js file
-  testMatch: [
-    '<rootDir>/packages/**/__tests__/**/*.[jt]s?(x)',
-    '<rootDir>/packages/**/?(*.)+(spec|test).[jt]s?(x)',
-  ],
-  testPathIgnorePatterns: ['<rootDir>/packages/runtime/test/test.js'],
 };
