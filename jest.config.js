f6bb06e206f61c1596f5778d4511a2a1a2dd221e
Felix Haus <felixhaus@gmail.com>
2021-06-23 22:02:11 +0200

    Move testPathIgnorePatterns to package.json

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,4 @@ module.exports = {
   testEnvironment: 'node',
   rootDir: './',
   globalSetup: '<rootDir>/test/jest.setup.ts',
-  // Exclude tests that don't run
-  testPathIgnorePatterns: ['<rootDir>/packages/runtime/test/test.js'],
 };
