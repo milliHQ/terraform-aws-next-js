b5df64813ccb447e32d8738c63e8edc256757d00
Felix Haus <felixhaus@gmail.com>
2021-09-18 17:20:52 +0200

    Fixes api page

--- test/fixtures/02-api/pages/api/sitemap/pages.js
+++ test/fixtures/02-api/pages/api/sitemap/pages.js
@@ -1,6 +1,5 @@
 export default function handler(_req, res) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/xml');
-
-  return `<?xml version="1.0" encoding="UTF-8"?><hello>world</hello>`;
+  res.end(`<?xml version="1.0" encoding="UTF-8"?><hello>world</hello>`);
 }
