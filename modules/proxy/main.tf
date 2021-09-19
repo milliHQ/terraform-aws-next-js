3ad455b9df0868c20b0f21e45e691ee59209e4dd
Felix Haus <felixhaus@gmail.com>
2021-09-19 20:32:37 +0200

    @millihq/terraform-next-proxy v0.8.0

--- modules/proxy/main.tf
+++ modules/proxy/main.tf
@@ -2,7 +2,7 @@ module "proxy_package" {
   source  = "milliHQ/download/npm"
   version = "2.0.0"
 
-  module_name    = "@dealmore/terraform-next-proxy"
+  module_name    = "@millihq/terraform-next-proxy"
   module_version = var.proxy_module_version
   path_to_file   = "dist.zip"
   use_local      = var.debug_use_local_packages
