4b1a14d37dc41cf3539600c4fd46a0f5800d6726
Felix Haus <felixhaus@gmail.com>
2021-09-21 12:11:37 +0200

    Rename package @dealmore/tf-next-runtime to @millihq/tf-next-runtime

--- packages/runtime/test/unit/export.test.js
+++ packages/runtime/test/unit/export.test.js
@@ -1,12 +1,12 @@
 describe('export', () => {
   it('should require by path main', async () => {
-    const main = require('@dealmore/tf-next-runtime');
+    const main = require('@millihq/tf-next-runtime');
     expect(main).toBeDefined();
   });
 
   it('should require by path dev-server relative to index', async () => {
-    const index = require('@dealmore/tf-next-runtime/dist/index.js');
-    const server = require('@dealmore/tf-next-runtime/dist/dev-server.js');
+    const index = require('@millihq/tf-next-runtime/dist/index.js');
+    const server = require('@millihq/tf-next-runtime/dist/dev-server.js');
     expect(index).toBeDefined();
     expect(server).toBeDefined();
   });
--- packages/tf-next/src/commands/build.ts
+++ packages/tf-next/src/commands/build.ts
@@ -1,5 +1,5 @@
-import { build } from '@dealmore/tf-next-runtime';
 import tmp from 'tmp';
+import { build } from '@millihq/tf-next-runtime';
 import {
   glob,
   Lambda,
