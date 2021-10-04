9ada8b83e24166ea619ebe6d4dda6db0f57ab60f
Felix Haus <ofhouse@users.noreply.github.com>
2021-10-04 18:49:42 +0200

    Use configuration aliases to pass global provider (#219)

--- provider.tf
+++
@@ -1,4 +0,0 @@
-# Proxy AWS provider for Lambda@Edge
-provider "aws" {
-  alias = "global_region"
-}
--- versions.tf
+++ versions.tf
@@ -1,11 +1,13 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 0.15"
 
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.43.0"
+      source                = "hashicorp/aws"
+      version               = ">= 3.43.0"
+      configuration_aliases = [aws.global_region]
     }
+
     random = {
       source  = "hashicorp/random"
       version = ">= 2.3.0"
