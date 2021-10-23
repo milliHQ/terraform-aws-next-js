575cc2951fd393add11ef319b31c6e6d72e86fd4
Felix Haus <felixhaus@gmail.com>
2021-10-23 19:51:46 +0200

    v0.10.1

--- modules/proxy/variables.tf
+++ modules/proxy/variables.tf
@@ -4,7 +4,7 @@
 
 variable "proxy_module_version" {
   type    = string
-  default = "0.10.0"
+  default = "0.10.1"
 }
 
 variable "lambda_default_runtime" {
--- modules/statics-deploy/variables.tf
+++ modules/statics-deploy/variables.tf
@@ -4,7 +4,7 @@ variable "static_files_archive" {
 
 variable "deploy_trigger_module_version" {
   type    = string
-  default = "0.10.0"
+  default = "0.10.1"
 }
 
 variable "expire_static_assets" {
