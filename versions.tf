6341aa2a189a226935030e0469fb4ca3608aef0c
Felix Haus <felixhaus@gmail.com>
2022-01-23 15:37:45 +0100

    Bump required aws provider from 3.43.0 to 3.64.0
    For using response_headers_policy_id which was introduced in #265

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = ">= 3.43.0"
+      version               = ">= 3.64.0"
       configuration_aliases = [aws.global_region]
     }
   }
